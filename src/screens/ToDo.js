f7e0554feac96f77e23bf89af99b04030403dd9b
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2019-06-14 16:57:41 +0530

    Task Update with dialoge input

--- src/screens/ToDo.js
+++ src/screens/ToDo.js
@@ -1,9 +1,16 @@
 import React, { PureComponent } from 'react';
 import { Alert ,Button, ScrollView, TouchableOpacity, View, Text, TouchableHighlight } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import DialogInput from 'react-native-dialog-input';
 
  export default class Todo extends PureComponent {
-					// toggle a todo as completed or not via update()
+
+					constructor(props) {
+						super(props);
+						this.state = {
+							isDialogVisible: false, 
+						};
+					}
 
 					toggleComplete() {
 						this.props.doc.ref.update({
@@ -17,13 +24,19 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome';
 						});
 					}
 
-					editTask(){
-					 
-			
-							// this.props.doc.ref.update({
-							// 	complete: !this.props.complete,
-							// });
+					showDialog(isShow) {
+						this.setState({ isDialogVisible: isShow });
 					}
+
+					sendInput(inputText) { 
+
+					 this.setState({ 
+							isDialogVisible: false,
+						}); 
+						this.props.doc.ref.update({
+							title: inputText,
+						});
+					} 
 
 					render() {
 						return (
@@ -36,8 +49,30 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome';
 								}}
 							>
 								<View style={{ flex: 8, flexDirection: 'row' }}>
-									<TouchableOpacity onPress={() => this.editTask()}>
-										<Icon style={{ color: 'red' }} size={20} name="edit" />
+									<DialogInput
+										isDialogVisible={this.state.isDialogVisible}
+										title={'Edit Task'}
+										// message={'Message for DialogInput #1'}
+										// hintInput={'HINT INPUT'}
+										initValueTextInput={this.props.title}
+										submitInput={inputText => {
+											this.sendInput(inputText);
+										}}
+										closeDialog={() => {
+											this.showDialog(false);
+										}}
+									/>
+									<TouchableOpacity
+										onPress={() => {
+											this.showDialog(true);
+										}}
+										style={{ padding: 3 }}
+									>
+										<Icon
+											style={{ left: 4, color: 'blue' }}
+											size={20}
+											name="edit"
+										/>
 									</TouchableOpacity>
 
 									<TouchableOpacity onPress={() => this.deleteTask()}>
@@ -49,13 +84,27 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome';
 									</TouchableOpacity>
 
 									<TouchableOpacity onPress={() => this.toggleComplete()}>
-										<Text style={{ left: 20 }}>{this.props.title}</Text>
+										<Text
+											style={{
+												left: 20,
+												textDecorationLine: this.props.complete
+													? 'line-through'
+													: 'none',
+												textDecorationStyle: 'solid',
+											}}
+										>
+											{this.props.title}
+										</Text>
 									</TouchableOpacity>
 									{/* <Button style={{ height:10  }} title={'delete'}/> */}
 								</View>
 								<View style={{ flex: 2 }}>
 									{this.props.complete && (
-										<Icon style={{ padding: 10 }} size={20} name="eye-slash" />
+										<Icon
+											style={{ padding: 10 }}
+											size={20}
+											name="eye-slash"
+										/>
 									)}
 								</View>
 							</View>
